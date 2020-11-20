542d9c5d7ffd512c0b2380b1343510841ebfcd3c
JHenrysson <56089745+JHenrysson@users.noreply.github.com>
2020-11-20 09:35:23 +0100

    Create ExampleOne.java

---
+++ src/ExampleOne.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ExampleOne {
+    public static void main (String []args){
+        double balance = 1000;
+    }
+    private static void withdrawMoney() {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+    }}
